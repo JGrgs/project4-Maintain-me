b7fbc83da5f24f84b482c8723319070a91ed889c
Jad Georges <jad.grgs@gmail.com>
2018-04-13 08:39:19 -0700

    add styling to home page

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -13,12 +13,12 @@ const Home = (props) => {
 				<div className="column-25">
 					<img className="image-circle" src={oilChangeImg} alt="oil change"/>
 				</div>
-				<div className="column-75">
+				<div className="column-75 text-column">
 					<p>Maintain me is an app created to help you track you vehicle's maintenance schedules. Every user gets to add their vehicle/vehicles into their profile and add their maintenance schedules into a table providing the maintenance type, due date/due at (miles).</p>
 				</div>
 			</div>
 			<div className="row">
-				<div className="column-25">
+				<div className="column-25 text-column">
 					<p>With the help of the google maps and yelp APIs, maintain me renders a map pinpointing all the mechanics around the user based on the user's location which is provided to us when a user signs up.</p>
 				</div>
 				<div className="column-75">
@@ -29,7 +29,7 @@ const Home = (props) => {
 				<div className="column-25">
 					<img className="image-circle" src={carImg} alt="car"/>
 				</div>
-				<div className="column-75">
+				<div className="column-75 text-column">
 					<p>Keep your vehicle in an excellent condition by always staying on top of the maintenance it needs.</p>
 				</div>
 			</div>
